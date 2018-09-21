99ddb161a45cfc23e63d87326eb2813299f29eeb
Ejnar Arechavala <ejnar123@gmail.com>
2018-09-21 04:22:18 -0400

    lightbox tweaks

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -9,13 +9,15 @@ class Banner extends Component {
             <div>
           <Paper style={{padding: "3em 2em", margin:"3vh auto"}}>
             <Grid container >
-              <Grid item md={6}>
+              <Grid item>
                 <div>
-                  <Typography variant="display2" color="inherit" gutterBottom>
+                  <Typography variant="display3" color="inherit" gutterBottom>
                     Comparaframe
                   </Typography>
                   <Typography variant="headline" color="inherit" paragraph>
                     This was made on September 19th, 2018 at Georgia Tech for my friend Sup who is really into his media collection.
+                    <br/>This tool allows Sup to add and remove images from a list and view them in a gallary.
+                    <br/>Sup will be using this to upload screenshots of original and encoded videos to check their quality.
                   </Typography>
                   <Typography variant="title" color="inherit">
                     Hope it works...
