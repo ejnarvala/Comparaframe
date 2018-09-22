8f53ea96e0c82fcf3af3d0995cd0743a0d8328f5
Ejnar Arechavala <ejnar123@gmail.com>
2018-09-21 23:38:21 -0400

    added link to source code and tooltip

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,7 +2,8 @@ import React from 'react';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
-
+import Button from '@material-ui/core/Button';
+import Tooltip from '@material-ui/core/Tooltip';
 
 const NavBar = (props) => {
 
@@ -10,9 +11,15 @@ const NavBar = (props) => {
     <div>
       <AppBar color="primary" position="fixed">
         <Toolbar>
-          <Typography color="inherit" variant="title">
+          <Typography color="inherit" variant="title" style={{flex: 1}}>
             Comparaframe
           </Typography>
+          <Tooltip title="Github Source">
+          <Button 
+            color="inherit"
+            href="https://github.com/ejnarvala/Comparaframe"
+            >Source</Button>
+          </Tooltip>
         </Toolbar>
       </AppBar>
     </div>
